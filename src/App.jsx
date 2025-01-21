f460ce9042c402bb2b7b3577a48b424dc9f38551
Oleg Shapoval <olegkazhan@gmail.com>
2025-01-22 00:47:41 +0200

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './img/logo.png'
 
 function App() {
 
@@ -7,16 +6,29 @@ function App() {
         <div className="header-top">
         <div className="container">
             <div className="header-top-row">
-                <a href="/main.jsx">
-                    <img className="logo"  src={logo} alt="logo"/>
+                <a href="">
+                    <img className="logo"  src="../src/img/burger.svg" alt="burger"/>
                 </a>
                 <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+                    <li><a href="">New Arrival</a></li>
+                    <li><a href="">Men</a></li>
+                    <li><a href="">Women</a></li>
+                    <li><a href="">Kids</a></li>
+                    <li><a href="">Contacts</a></li>
                 </ul>
+                <a href="">
+                    <img className="logo"  src="../src/img/logo.png" alt="logo"/>
+                </a>
+                <div className="png-row">
+                    <img className="search"
+                         src="../src/img/search.png" alt="search"/>
+                    <img
+                        className="account"
+                        src="../src/img/account.png" alt="account"/>
+                    <img
+                        className="cart"
+                         src="../src/img/cart.png" alt="cart"/>
+                </div>
             </div>
         </div>
         </div>
