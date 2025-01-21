6db20b49fd8db4cb341fc140ce0b9fd38022b255
Oleg Shapoval <olegkazhan@gmail.com>
2025-01-22 00:07:37 +0200

    logo + li -

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import logo from './img/logo.png'
+
+function App() {
+
+  return (
+    <>
+        <div className="header-top">
+        <div className="container">
+            <div className="header-top-row">
+                <a href="/main.jsx">
+                    <img className="logo"  src={logo} alt="logo"/>
+                </a>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+        </div>
+
+    </>
+  )
+}
+
+export default App
