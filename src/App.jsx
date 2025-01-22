977d4810a9f29747d3ec5843ab9139a7b6139c47
Oleg Shapoval <olegkazhan@gmail.com>
2025-01-22 15:43:36 +0200

    Header-Top - component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
-import { FiSearch } from "react-icons/fi";
+
+import HeaderTop from "../components/Header-Top.jsx";
 
 
 function App() {
 
   return (
     <>
-        <div className="header-top">
-        <div className="container">
-            <div className="header-top-row">
-                <a href="">
-                    <img className="burger"  src="../src/img/burger.svg" alt="burger"/>
-                </a>
-            </div>
-            <div className="center">
-                <a href="">
-                    <img className="logo"  src="../src/img/logo.png" alt="logo"/>
-                </a>
-            </div>
-
-                <ul className="nav">
-                    <li><a href="">New Arrival</a></li>
-                    <li><a href="">Men</a></li>
-                    <li><a href="">Women</a></li>
-                    <li><a href="">Kids</a></li>
-                </ul>
-
-            <div className="png-row">
-                    <FiSearch className="search" size={32} alt="Search"/>
-                    <img
-                        className="account"
-                        src="../src/img/account.png" alt="account"/>
-                    <img
-                        className="cart"
-                         src="../src/img/cart.png" alt="cart"/>
-                </div>
-
-        </div>
-        </div>
+        <HeaderTop/>
 
     </>
   )
