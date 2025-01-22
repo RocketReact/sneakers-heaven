d72b30a8618d8a82a3d8daec2283f21099b8b4d1
Oleg Shapoval <olegkazhan@gmail.com>
2025-01-22 15:12:21 +0200

    ready burger, search, cart, account

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { FiSearch } from "react-icons/fi";
+
 
 function App() {
 
@@ -7,21 +9,24 @@ function App() {
         <div className="container">
             <div className="header-top-row">
                 <a href="">
-                    <img className="logo"  src="../src/img/burger.svg" alt="burger"/>
+                    <img className="burger"  src="../src/img/burger.svg" alt="burger"/>
                 </a>
-                <ul>
+            </div>
+            <div className="center">
+                <a href="">
+                    <img className="logo"  src="../src/img/logo.png" alt="logo"/>
+                </a>
+            </div>
+
+                <ul className="nav">
                     <li><a href="">New Arrival</a></li>
                     <li><a href="">Men</a></li>
                     <li><a href="">Women</a></li>
                     <li><a href="">Kids</a></li>
-                    <li><a href="">Contacts</a></li>
                 </ul>
-                <a href="">
-                    <img className="logo"  src="../src/img/logo.png" alt="logo"/>
-                </a>
-                <div className="png-row">
-                    <img className="search"
-                         src="../src/img/search.png" alt="search"/>
+
+            <div className="png-row">
+                    <FiSearch size={32} alt="Search"/>
                     <img
                         className="account"
                         src="../src/img/account.png" alt="account"/>
@@ -29,7 +34,7 @@ function App() {
                         className="cart"
                          src="../src/img/cart.png" alt="cart"/>
                 </div>
-            </div>
+
         </div>
         </div>
 
