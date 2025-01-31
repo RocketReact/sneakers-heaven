04ab24f26d8f2b45fdccbe8d131feaf5d17d8ca0
Oleg Shapoval <olegkazhan@gmail.com>
2025-01-31 22:26:48 +0200

    products greed

--- components/Header-Top/Header-Top.jsx
+++ components/Header-Top/Header-Top.jsx
@@ -1,27 +1,39 @@
 import './Header-Top.css';
 import {FiSearch} from "react-icons/fi";
+import {Link, Route, Routes} from "react-router-dom";
+import Home from "../Pages/Home/Home.jsx";
+import About from "../Pages/About/About.jsx";
+import Man from "../Pages/Man/Man.jsx";
+import Woman from "../Pages/Woman/Woman.jsx";
+import ShopAll from "../Pages/Shop All/ShopAll.jsx";
+import Contact from "../Pages/Contact/Contact.jsx";
+
 
 const HeaderTop = () => {
 
     return (
+
         <div className="header-top">
-            <div className="container">
+            <nav className="container">
                 <div className="center">
-                    <a href="">
-                        <img className="logo"  src="../../src/img/logo.png" alt="logo"/>
-                    </a>
+                    <Link to="/">
+                        <img className="logo" src="../../src/img/logo.png" alt="logo" />
+                    </Link>
                 </div>
 
                 <ul className="nav">
-                    <li><a href="">New Arrival</a></li>
-                    <li><a href="">Men</a></li>
-                    <li><a href="">Women</a></li>
-                    <li><a href="">Kids</a></li>
+                    <li><Link to ="/"> Home </Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/man">Man</Link></li>
+                    <li><Link to="/wooman">Woman</Link></li>
+                    <li><Link to="/shop-all">Shop All</Link></li>
+
                 </ul>
 
                 <div className="png-row">
 
-                    <FiSearch className="search" size={32} alt="Search"/>
+                    <FiSearch className="search" fontSize={32} alt="Search"/>
                     <img
                         className="account"
                         src="../../src/img/account.png" alt="account"/>
@@ -29,16 +41,30 @@ const HeaderTop = () => {
                         className="cart"
                         src="../../src/img/cart.png" alt="cart"/>
                     <div className="header-top-row">
-                        <a href="">
+                        <Link to="">
                             <img className="burger"  src="../../src/img/burger.svg" alt="burger"/>
-                        </a>
+                        </Link>
                     </div>
                 </div>
+            </nav>
 
 
-            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/man" element={<Man/>} />
+                <Route path="/woman" element={<Woman />} />
+                <Route path="/shop-all" element={<ShopAll/>} />
+
+            </Routes>
+
+
+
         </div>
-    );
+    ) ;
 };
 
 export default HeaderTop;
+
+
