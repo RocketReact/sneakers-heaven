5fcb90b51d48eb595d512c221a4e155289daf905
Oleg Shapoval <olegkazhan@gmail.com>
2025-02-09 23:45:10 +0200

    searching

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontSize:{
+                '4.5xl': '2.75 rem'
+            },
+        },
+    },
+    plugins: [],
+};
