455f019ef7f00c30a34375b0c1b361d7b2d1f158
Oleg Shapoval <olegkazhan@gmail.com>
2025-02-10 16:14:05 +0200

    ProductGrid Correct addToCart

--- src/components/ProductsGrid/ProductsGrid.jsx
+++ src/components/ProductsGrid/ProductsGrid.jsx
@@ -74,8 +74,6 @@ export default function ProductsGrid() {
                                 >
                                     Add to cart
                                 </button>
-
-
                         </div>
                     ))}
                 </div>
