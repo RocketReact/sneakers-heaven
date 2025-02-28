7d492aa76f40266b2b7e92f1d5369d20161c1de9
Oleg Shapoval <olegkazhan@gmail.com>
2025-02-28 18:35:48 +0200

    banner shop click - navigate to shop all

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,16 @@
 import "./Banner.css";
-
+import {useNavigate} from "react-router-dom";
 const Banner = () => {
+    const navigate = useNavigate();
     return (
             <div className="banner-container">
 
                 <div className="banner-content">
                     <h2> Buy the best running sneakers </h2>
                     <p> We are present in Germany 10 years </p>
-                    <button className="banner-button">Shop</button>
+                    <button
+                        onClick= {() => navigate('/shop-all')}
+                        className="banner-button">Shop</button>
                 </div>
             </div>
 
