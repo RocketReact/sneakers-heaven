b23e84ddd01ce5db7445810621a13c990644bea3
Oleg Shapoval <olegkazhan@gmail.com>
2025-03-01 14:18:43 +0200

    Notification Enail Registration

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,36 @@
 import { FormProvider, useForm } from "react-hook-form";
 import {Email} from '../TextInput/TextInputHtml.jsx'
 import {addUserData} from "../../data/userRegisterData.js";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
 
 export default function Login () {
     const methods = useForm()
     const { handleSubmit, reset } = methods;
     const storedData = JSON.parse(localStorage.getItem("userRegisterData")) || [];
+
+    const notifySuccess = () => {
+        toast.success('🎉 Your email was successfully registered!Check your mailbox to complete registration.', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        })
+    };
+    const notifyError = () => {
+        toast.error('Email address already exists! Choose another email!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        })
+    };
 
     const onSubmit = (data) => {
         const newUser = {
@@ -14,12 +39,12 @@ export default function Login () {
         };
 
         if (storedData.some(user => user.email === newUser.email)) {
-            alert('Email address already exists! Choose another email!');
+            notifyError();
         } else {
             addUserData(newUser);
             localStorage.setItem("userRegisterData", JSON.stringify([...storedData, newUser]));
-            alert('🎉 Your email was successfully registered! Check your mailbox to complete registration.');
-            reset();
+            reset()
+            notifySuccess()
 
         }
     };
@@ -27,13 +52,13 @@ export default function Login () {
     const onError = (errors) => {
         console.log("Form Errors:", errors);
     };
+
     return (
 
         <FormProvider {...methods} >
 
             <form onSubmit={handleSubmit(onSubmit, onError)}
                   className='w-100 justify-self-center mt-20'>
-
                 <h1 className='text-2xl'>Enter your email to join us or sign in.</h1>
                 <Email/>
                 <div className='justify-self-end mt-10'>
@@ -42,10 +67,10 @@ export default function Login () {
                     className='
                 bg-black rounded-full p-3 pr-5 pl-5
                 text-white font-extralight hover:bg-gray-400
-                hover:cursor-pointer active:scale-90 duration-200
-                '>Continue</button>
+                hover:cursor-pointer active:scale-90 duration-200'
+                > Continue </button>
                 </div>
-
+                <ToastContainer/>
             </form>
         </FormProvider>
     );
