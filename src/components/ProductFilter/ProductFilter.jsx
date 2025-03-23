b25c9ef4ef484923c02c42e6ed2fbee6a21ecbf4
Oleg Shapoval <olegkazhan@gmail.com>
2025-03-23 07:48:48 +0200

    ProductFilter.jsx Comments

--- src/components/ProductFilter/ProductFilter.jsx
+++ src/components/ProductFilter/ProductFilter.jsx
@@ -1,10 +1,16 @@
 import {useDispatch, useSelector} from "react-redux";
 import {setCategoryFilter} from "../../store/productSlice/productSlice.js";
 
-export default function ProductFilter  ({categories}) {
+/**
+ * Category filter dropdown component
+ * @param {string[]} categories - Available product categories
+ */
+export default function ProductFilter({categories}) {
     const dispatch = useDispatch();
-        const selectedCategory = useSelector ((state) => state.products.filteredCategory)
+    // Get currently selected category from Redux store
+    const selectedCategory = useSelector((state) => state.products.filteredCategory)
 
+    // Update filter when selection changes
     const handleFilterChangeCategory = (e) => {
         dispatch(setCategoryFilter(e.target.value));
     }
@@ -16,17 +22,16 @@ export default function ProductFilter  ({categories}) {
             <select
                 value={selectedCategory || ''}
                 onChange={handleFilterChangeCategory}
-                className='mt-1 block w-full  p-2 border border-gray-300 rounded-md shadow-sm'>
+                className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm'>
 
+                {/* Default option shows all products */}
                 <option value=''> All Category </option>
+
+                {/* Map through available categories */}
                 {categories.map(category => (
                     <option key={category} value={category}> {category}</option>
                 ))}
-
-
             </select>
-
         </div>
     );
 };
-
