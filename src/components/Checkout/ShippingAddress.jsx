d45c0ed0a23871f944b5b3d008f7ba157095d09b
Oleg Shapoval <olegkazhan@gmail.com>
2025-03-23 06:50:03 +0200

    ShippingAddress.jsx Comments

--- src/components/Checkout/ShippingAddress.jsx
+++ src/components/Checkout/ShippingAddress.jsx
@@ -1,13 +1,19 @@
 import {useSelector} from "react-redux";
 
+/**
+ * Displays customer shipping address information
+ * @param {boolean} isHidden - Hide shipping speed section when true
+ * @param {boolean} textSmall - Use smaller text styling when true
+ */
 export default function ShippingAddress({isHidden, textSmall}) {
+    // Get customer data from Redux store
     const {customerData} = useSelector(state => state.checkout);
-    return <ul className={`${textSmall? ' text-sm text-gray-600': ' text-base'} text-left flex flex-col font-extralight`}
 
-      >
+    return <ul className={`${textSmall? ' text-sm text-gray-600': ' text-base'} text-left flex flex-col font-extralight`}>
+        {/* Map through customer data safely with array check */}
         {Array.isArray(customerData) && customerData?.map((item) => (
             <li key={item.id}>
-
+                {/* Address details */}
                 <p className='font-normal'>Shipping Address</p>
                 <div>{item.email}</div>
                 <div>{item.firstName}</div>
@@ -17,6 +23,7 @@ export default function ShippingAddress({isHidden, textSmall}) {
                 <div>{item.postalCode}</div>
                 <div>{item.phoneNumber}</div>
 
+                {/* Conditionally rendered shipping speed section */}
                 <div className={isHidden ? 'hidden' : ''}>
                     <p className= 'font-normal mt-2'>Shipping Speed</p>
                     <div>{item.deliverySpeed}</div>
