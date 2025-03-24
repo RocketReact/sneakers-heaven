7c65dd71325d0b53cd1bfa88d22d083444b26c6f
Oleg Shapoval <olegkazhan@gmail.com>
2025-03-24 13:45:06 +0200

    Ready Footer in mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import SectionFooter from "./SectionFooter.jsx";
-
+import {Link} from "react-router-dom";
 
 
 export default function Footer() {
 
-    return <footer className='mx-10 lg:mx-20 '>
-        <div className='flex flex-col'>
-
-            <SectionFooter resources={true} textSection='Resources'
+    return <footer className='mx-10 lg:mx-20 mt-10'>
+        <div className='flex flex-col mb-20 '>
+            <div className='flex flex-col lg:items-center lg:justify-between lg:flex-row '>
+            <SectionFooter textSection='Resources'
                            resourcesLinks={[
                                {text: 'Gift Cards', to: ''},
                                {text: 'Find a Store', to: ''},
@@ -16,40 +16,49 @@ export default function Footer() {
                            ]} />
 
 
-            <SectionFooter help={true} textSection='Help'
+            <SectionFooter textSection='Help'
                            resourcesLinks={[
                                {text: 'Get Help', to: ''},
                                {text: 'Order Status', to: ''},
                                {text: 'Shipping and Delivery', to: ''},
                                {text: 'Returns', to: ''},
-                               {text: 'O', to: ''},
-                               {text: 'Find a Store', to: ''},
-                               {text: 'Membership', to: ''},
-                               {text: 'Site Feedback', to: ''}
+                               {text: 'Order Cancellation', to: ''},
+                               {text: 'Payment Options', to: ''},
+                               {text: 'Gift Card Balance', to: ''},
+                               {text: 'Contact us', to: '/contact'}
                            ]}
             />
-            <SectionFooter company={true} textSection='Company'
+            <SectionFooter textSection='Company'
                            resourcesLinks={[
-                               {text: 'Gift Cards', to: ''},
-                               {text: 'Find a Store', to: ''},
-                               {text: 'Membership', to: ''},
-                               {text: 'Site Feedback', to: ''},
-                               {text: 'Gift Cards', to: ''},
-                               {text: 'Find a Store', to: ''},
+                               {text: 'About Sneakers Heaven', to: ''},
+                               {text: 'News', to: ''},
+                               {text: 'Careers', to: ''},
+                               {text: 'Investors', to: ''},
+                               {text: 'Purpose', to: ''},
+                               {text: 'Sustainability', to: ''},
                            ]}
             />
-            <SectionFooter promotions={true} textSection='Promotions'
+            <SectionFooter textSection='Promotions'
                            resourcesLinks={[
-                               {text: 'Gift Cards', to: ''},
-                               {text: 'Find a Store', to: ''},
-                               {text: 'Membership', to: ''},
-                               {text: 'Site Feedback', to: ''},
-                               {text: 'Gift Cards', to: ''},
+                               {text: 'Student', to: ''},
+                               {text: 'Military', to: ''},
+                               {text: 'Teacher', to: ''},
+                               {text: 'First Responders & Medical Professionals', to: ''},
+                               {text: 'Birthday', to: ''},
                            ]}
             />
+            </div>
 
+          <div>
+            <hr className='lg:hidden' />
+            <p className='my-4'>© 2025 Sneakers Heaven, Online Store. All Rights Reserved</p>
+            <div className='flex flex-col gap-1 text-cyan-800'>
+              <Link to=''> <h5>Terms of Sale</h5> </Link>
+              <Link to=''> <h5>Terms of Use</h5> </Link>
+              <Link to=''> <h5>Nike Privacy Policy</h5> </Link>
+            </div>
+          </div>
         </div>
     </footer>
-
 
 }
--- src/components/Footer/SectionFooter.jsx
+++ src/components/Footer/SectionFooter.jsx
@@ -2,43 +2,42 @@ import {Link} from "react-router-dom";
 import {useState} from "react";
 
 export default function SectionFooter({
-                                          resources= false,
-                                          help = false,
-                                          company = false,
-                                          promotions = false,
                                           resourcesLinks = [{text: '', to:''}],
                                           textSection = ''
                                       }) {
 
-    const [isOpen, setOpen] = useState({resources, help, company, promotions});
+    const [isOpen, setOpen] = useState(false);
 
-    const fnToggle = (id) => {
-        setOpen(prev => ({...prev, [id]: !prev[id],
-        }))
-    }
+    const fnToggle = () => {
+        setOpen(!isOpen);
+    };
 
 
     return <div className='flex flex-col'>
         <hr className='lg:hidden' />
+
         <div
             className='flex flex-row items-center justify-between
                 lg:justify-center mt-6 hover:cursor-pointer '
-            onClick={() => fnToggle('resources')}
+            onClick={fnToggle}
         >
             <h3>{textSection}</h3>
+
             <span
-                className={`${isOpen.resources? 'rotate-180' : 'rotate-0'}
+                className={`${isOpen? 'rotate-180' : 'rotate-0'}
                            text-gray-400 hover:text-gray-600
                             hover:cursor-pointer mt-1 lg:hidden
-                            transition-transform ease-out duration-500
+                            transition-transform duration-500 ease-out 
                            `}
             >
                 ▼
                </span>
+
         </div>
 
+
         <div className={`transition-all duration-500 ease-out overflow-hidden ${
-            isOpen.resources ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'
+            isOpen? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'
         } mb-7`}
         >
             <ul className='space-y-3 mt-4 text-cyan-800'>
